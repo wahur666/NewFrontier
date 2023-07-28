3c3cd3c9bb5daf1fcab723f725c31ea122de5df5
wahur666 <imre.secodi@gmail.com>
2023-07-28 23:24:41 +0200

    add harvesting logic

--- scripts/ResourceNode2D.cs
+++ scripts/ResourceNode2D.cs
@@ -1,12 +1,17 @@
-﻿using Godot;
+using Godot;
 using NewFrontier.scripts.helpers;
 
 namespace NewFrontier.scripts; 
 
 public partial class ResourceNode2D : Node2D {
 
-    public ResourceType Resource;
-    public int CurrentResource;
-    [Export] public int MaxResource = 500;
+	public ResourceType Resource;
+	public int CurrentResource;
+	[Export] public int MaxResource = 500;
+
+	public void UpdateScale() {
+		var newScale = (float)CurrentResource / MaxResource;
+		Scale = new Vector2(newScale, newScale);
+	}
 
 }
