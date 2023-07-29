5dfd8937fa5ad93108391905e423d474b0b6a80e
wahur666 <imre.secodi@gmail.com>
2023-07-29 15:09:27 +0200

    add gas resource

--- scripts/ResourceNode2D.cs
+++ scripts/ResourceNode2D.cs
@@ -8,10 +8,16 @@ public partial class ResourceNode2D : Node2D {
 	public ResourceType Resource;
 	public int CurrentResource;
 	[Export] public int MaxResource = 500;
+	[Export] public float BaseScale = 1f;
+
+	public override void _Ready() {
+		CurrentResource = MaxResource;
+		UpdateScale();
+	}
 
 	public void UpdateScale() {
 		var newScale = (float)CurrentResource / MaxResource;
-		Scale = new Vector2(newScale, newScale);
+		Scale = new Vector2(newScale, newScale) * BaseScale;
 	}
 
 }
