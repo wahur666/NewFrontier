5d7caae94295ba0a0a3b914038f9234b17cb490b
wahur666 <imre.secodi@gmail.com>
2023-08-07 22:02:33 +0200

    cleanup buildings, and make it snap to any building

--- scripts/BuildingNode2D.cs
+++ scripts/BuildingNode2D.cs
@@ -1,17 +1,21 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using NewFrontier.scripts;
 
 public partial class BuildingNode2D : Node2D {
 	[Export] public int Wide = 1;
 	private int baseAngle = 30;
-	private Planet _planet;
+	// private Planet _planet;
 	private bool _snapToPlanet = false;
 	private Sprite2D _sprite;
-	
+	private List<Planet> _planets;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		_planet = GetTree().CurrentScene.GetNode<Planet>("WetPlanet");
+		// _planet = GetTree().CurrentScene.GetNode<Planet>("WetPlanet");
+		_planets = GetTree().GetNodesInGroup("planet").Select(planet => planet as Planet).ToList();
 		_sprite = GetNode<Sprite2D>("Sprite2D");
 	}
 
@@ -33,16 +37,17 @@ public partial class BuildingNode2D : Node2D {
 		// }
 
 		var pos = GetGlobalMousePosition();
-		var distance = pos.DistanceTo(_planet.GlobalPosition);
-		if (Math.Abs(distance - _planet.Radius) < 20) {
-			var place = CalculatePlace(_planet, pos.X, pos.Y);
+		if (_planets.Any(planet => DistanceFn(pos, planet))) {
+			var planet = _planets.First(planet => DistanceFn(pos, planet));
+			var place = CalculatePlace(planet, pos.X, pos.Y);
 			GD.Print(place.Join());
 		} else {
 			GlobalPosition = pos - (_sprite.Offset * new Vector2(2.5f, 2.5f));
 			RotationDegrees = 0;
 		}
-
 	}
+
+	private static bool DistanceFn(Vector2 mousePos, Planet planet) => Math.Abs(mousePos.DistanceTo(planet.GlobalPosition) - planet.Radius) < 20;
 
 	public int[] CalculatePlace(Planet planet, double x, double y) {
 		double angle = Math.Atan2(y - planet.GlobalPosition.Y, x - planet.GlobalPosition.X);
@@ -54,7 +59,6 @@ public partial class BuildingNode2D : Node2D {
 		double posY = planet.GlobalPosition.Y;
 		RotationDegrees = place * 30 - 90 + (Wide % 2 == 0 ? 16 : 0);
 		GlobalPosition = new Vector2((float)posX, (float)posY);
-		GD.Print(GlobalPosition);
 
 		int[] hoverPos;
 		switch (Wide) {
@@ -62,17 +66,17 @@ public partial class BuildingNode2D : Node2D {
 				hoverPos = new[] { place };
 				break;
 			case 2: {
-					int prev = place - 1 < 0 ? 11 : place - 1;
-					int next = place > 11 ? 0 : place;
-					hoverPos = new[] { prev, next };
-					break;
-				}
+				int prev = place - 1 < 0 ? 11 : place - 1;
+				int next = place > 11 ? 0 : place;
+				hoverPos = new[] { prev, next };
+				break;
+			}
 			case 3: {
-					int prev = place - 1 < 0 ? 11 : place - 1;
-					int next = place + 1 > 11 ? 0 : place + 1;
-					hoverPos = new[] { prev, place, next };
-					break;
-				}
+				int prev = place - 1 < 0 ? 11 : place - 1;
+				int next = place + 1 > 11 ? 0 : place + 1;
+				hoverPos = new[] { prev, place, next };
+				break;
+			}
 			default:
 				hoverPos = Array.Empty<int>();
 				break;
