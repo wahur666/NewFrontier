dc07fe8b0c83cb5de53482ab34fbdf8faa3eb4ab
Szecsődi Imre <imre.secodi@gmail.com>
2023-10-24 21:15:50 +0200

    add random point function

--- scripts/helpers/MapHelpers.cs
+++ scripts/helpers/MapHelpers.cs
@@ -1,11 +1,20 @@
-﻿using Godot;
+﻿using System;
+using Godot;
 using NewFrontier.scripts.Model;
 
-namespace NewFrontier.scripts.helpers; 
+namespace NewFrontier.scripts.helpers;
 
 public class MapHelpers {
 	public static Vector2 GridCoordToGridPointPos(Vector2 gridCoordinate, float size) => gridCoordinate * size;
 	public static Vector2 GridCoordToGridCenterPos(Vector2 gridCoordinate, float size) => gridCoordinate * size + new Vector2(size / 2, size / 2);
+	public static Vector2 GridCoordToGridRandomPos(Vector2 gridCoordinate, float size) {
+		var rand = new Random();
+		return gridCoordinate * size + 
+			new Vector2(size / 4 + size / 2 * (float)rand.NextDouble(), 
+						size / 4 + size / 2 * (float)rand.NextDouble());
+	}
+
+
 	public static Vector2 NodeToPos(GameNode node, float size) => node.Position * size + new Vector2(size / 2, size / 2);
 	public static Vector2 PosToGrid(Vector2 pos, float size) => new((int)pos.X / size, (int)pos.Y / size);
 }
