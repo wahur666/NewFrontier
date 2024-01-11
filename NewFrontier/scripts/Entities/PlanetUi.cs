80d39a4c9fdf83ba3d7ee409a92675f0c361e328
wahur666 <imre.secodi@gmail.com>
2024-01-11 09:24:01 +0100

    refactor planet ui, to a separate scene

--- NewFrontier/scripts/Entities/PlanetUi.cs
+++ NewFrontier/scripts/Entities/PlanetUi.cs
@@ -11,22 +11,9 @@ public partial class PlanetUi : Control {
 
 	private TextureRect _planetTexture;
 
-	private Label _oreLabel;
-	private Label _oreMiningLabel;
-	private Label _gasLabel;
-	private Label _gasMiningLabel;
-	private Label _crewLabel;
-	private Label _crewMiningLabel;
-
-	private PanelContainer _oreContainer;
-	private HBoxContainer _oreProgressContainer;
-
-
-	private PanelContainer _gasContainer;
-	private HBoxContainer _gasProgressContainer;
-
-	private PanelContainer _crewContainer;
-	private HBoxContainer _crewProgressContainer;
+	private ResourceUiContainer _oreContainer;
+	private ResourceUiContainer _gasContainer;
+	private ResourceUiContainer _crewContainer;
 
 	public override void _Ready() {
 		_planetName = GetNode<Label>("PanelContainer/VBoxContainer/PanelContainer/VBoxContainer/PlanetName");
@@ -35,41 +22,14 @@ public partial class PlanetUi : Control {
 		_planetTexture =
 			GetNode<TextureRect>(
 				"PanelContainer/VBoxContainer/HBoxContainer/PanelContainer/PanelContainer2/PanelContainer/PlanetTexture");
-		_oreLabel = GetNode<Label>(
-			"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/OreContainer/HBoxContainer/PanelContainer2/VBoxContainer/HBoxContainer/OreLabel");
-		_oreMiningLabel =
-			GetNode<Label>(
-				"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/OreContainer/HBoxContainer/PanelContainer2/VBoxContainer/HBoxContainer/OreMinigLabel");
-		_gasLabel = GetNode<Label>(
-			"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/GasContainer/HBoxContainer/PanelContainer2/VBoxContainer/HBoxContainer/GasLabel");
-		_gasMiningLabel =
-			GetNode<Label>(
-				"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/GasContainer/HBoxContainer/PanelContainer2/VBoxContainer/HBoxContainer/GasMiningLabel");
-		_crewLabel =
-			GetNode<Label>(
-				"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/CrewContainer/HBoxContainer/PanelContainer2/VBoxContainer/HBoxContainer/CrewLabel");
-		_crewMiningLabel =
-			GetNode<Label>(
-				"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/CrewContainer/HBoxContainer/PanelContainer2/VBoxContainer/HBoxContainer/CrewMiningLabel");
 
 		_oreContainer =
-			GetNode<PanelContainer>("PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/OreContainer");
+			GetNode<ResourceUiContainer>("PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/OreContainer");
 		_gasContainer =
-			GetNode<PanelContainer>(
-				"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/GasContainer");
+			GetNode<ResourceUiContainer>("PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/OreContainer");
 		_crewContainer =
-			GetNode<PanelContainer>(
-				"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/CrewContainer");
+			GetNode<ResourceUiContainer>("PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/CrewContainer");
 
-		_oreProgressContainer =
-			GetNode<HBoxContainer>(
-				"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/OreContainer/HBoxContainer/PanelContainer2/VBoxContainer/OreProgressContainer");
-		_gasProgressContainer =
-			GetNode<HBoxContainer>(
-				"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/GasContainer/HBoxContainer/PanelContainer2/VBoxContainer/GasProgressContainer");
-		_crewProgressContainer =
-			GetNode<HBoxContainer>(
-				"PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/CrewContainer/HBoxContainer/PanelContainer2/VBoxContainer/CrewProgressContainer");
 	}
 
 	public void UpdateLabels(Planet planet) {
@@ -80,44 +40,33 @@ public partial class PlanetUi : Control {
 			PlanetType.GasGiant => "Gas Giant",
 			PlanetType.Swamp => "Swamp Planet",
 		};
-		_oreLabel.Text = $"Ore: {planet.PlanetStats.CurrentOre}/{planet.PlanetStats.MaxOre}";
-		_gasLabel.Text = $"Gas: {planet.PlanetStats.CurrentGas}/{planet.PlanetStats.MaxGas}";
-		_crewLabel.Text = $"Crew: {planet.PlanetStats.CurrentCrew}/{planet.PlanetStats.MaxCrew}";
+		_oreContainer.UpdateResourceLabel(planet.PlanetStats.CurrentOre, planet.PlanetStats.MaxOre);
+		_gasContainer.UpdateResourceLabel(planet.PlanetStats.CurrentGas, planet.PlanetStats.MaxGas);
+		_crewContainer.UpdateResourceLabel(planet.PlanetStats.CurrentCrew, planet.PlanetStats.MaxCrew);
 
 		var count = planet.Buildings.Count(building => building is Refinery);
 		switch (planet.PlanetType) {
 			case PlanetType.Earth:
-				_oreMiningLabel.Text = $"{count * 150}/min";
-				UpdateProgressContainer(_oreContainer, planet.PlanetStats.CurrentOre, planet.PlanetStats.MaxOre);
-				_gasMiningLabel.Text = $"{count * 150}/min";
-				UpdateProgressContainer(_gasContainer, planet.PlanetStats.CurrentGas, planet.PlanetStats.MaxGas);
-				UpdateProgressContainer(_crewContainer, planet.PlanetStats.CurrentCrew, planet.PlanetStats.MaxCrew);
+				_oreContainer.UpdateResourceMiningLabel(count * 150);
+				_oreContainer.UpdateProgressContainer(planet.PlanetStats.CurrentOre, planet.PlanetStats.MaxOre);
+				_gasContainer.UpdateResourceMiningLabel(count * 150);
+				_gasContainer.UpdateProgressContainer(planet.PlanetStats.CurrentGas, planet.PlanetStats.MaxGas);
+				// UpdateProgressContainer(_crewProgressContainer, planet.PlanetStats.CurrentCrew, planet.PlanetStats.MaxCrew);
 				break;
 			case PlanetType.Moon:
-				_oreMiningLabel.Text = $"{count * 150}/min";
+				_oreContainer.UpdateResourceMiningLabel(count * 150);
 				_gasContainer.Modulate = Color.Color8(0, 0, 0, 0);
 				_crewContainer.Modulate = Color.Color8(0, 0, 0, 0);
 				break;
 			case PlanetType.GasGiant:
 				_oreContainer.Modulate = Color.Color8(0, 0, 0, 0);
-				_gasMiningLabel.Text = $"{count * 150}/min";
+				_gasContainer.UpdateResourceMiningLabel(count * 150);
 				_crewContainer.Modulate = Color.Color8(0, 0, 0, 0);
 				break;
 			case PlanetType.Swamp:
 				_oreContainer.Modulate = Color.Color8(0, 0, 0, 0);
 				_gasContainer.Modulate = Color.Color8(0, 0, 0, 0);
 				break;
-		}
-		
-	}
-
-	private static void UpdateProgressContainer(PanelContainer panelContainer, int currentValue, int maxValue) {
-		var length = panelContainer.GetChildren().Count;
-		var availableResourceSteps = (int)(length * currentValue / (double)maxValue);
-		var children = panelContainer.GetChildren().ToList().Select(rect => rect as ColorRect).ToList();
-		for (int i = 0; i < length; i++){
-			var alpha = (byte)(i > availableResourceSteps ? 0 : 255); 
-			children[i].Modulate = Color.Color8(0, 0, 0, alpha);
 		}
 	}
 }
